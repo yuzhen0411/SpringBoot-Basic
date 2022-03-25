aa07be48ed8cd204ab8110ddd7e8eef7b3d312e3
yuzhen0411 <reliy1012@gmail.com>
2022-03-25 18:01:01 +0800

    finish add employee

--- src/main/java/com/yilia/springboot/controller/EmployeeController.java
+++ src/main/java/com/yilia/springboot/controller/EmployeeController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.yilia.springboot.dao.DepartmentDao;
 import com.yilia.springboot.dao.EmployeeDao;
+import com.yilia.springboot.entities.Department;
 import com.yilia.springboot.entities.Employee;
 
 @Controller
@@ -16,6 +19,9 @@ public class EmployeeController {
 	@Autowired
 	EmployeeDao employeeDao;
 	
+	@Autowired
+	DepartmentDao departmentDao;
+	
 	@GetMapping("/emps")
 	public String list(Model model) {
 		Collection<Employee> employees = employeeDao.getAll();
@@ -23,8 +29,19 @@ public class EmployeeController {
 		return "emp/list";
 	}
 	
+	//turn to add page
 	@GetMapping("/emp")
-	public String toAddPage() {
+	public String toAddPage(Model model) {
+		Collection<Department> departments = departmentDao.getDepartments();
+		model.addAttribute("depts", departments);
 		return("emp/add");
+	}
+	
+	//add employee
+	@PostMapping("/emp")
+	public String addEmp(Employee employee) {
+		System.out.println("已新增員工: " + employee);
+		employeeDao.save(employee);
+		return"redirect:/emps";
 	}
 }
