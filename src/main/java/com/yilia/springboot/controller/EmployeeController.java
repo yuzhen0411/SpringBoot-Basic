2bd3c8de1c2309ca41fdb8b410d4c7c9bc07acce
yuzhen0411 <reliy1012@gmail.com>
2022-03-30 08:23:18 +0800

    finish error page pre_knowledge

--- src/main/java/com/yilia/springboot/controller/EmployeeController.java
+++ src/main/java/com/yilia/springboot/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,12 @@ public class EmployeeController {
 	public String updateEmployee(Employee employee) {
 		System.out.println("更新後員工資訊: " + employee);
 		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+	
+	@DeleteMapping("/emp/{id}")
+	public String deleteEmployee(@PathVariable("id") Integer id) {
+		employeeDao.delete(id);
 		return "redirect:/emps";
 	}
 }
